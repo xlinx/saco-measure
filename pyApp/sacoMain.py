3bf0e2a66608ca2ccef18a3d3a508c3f500523b8
xGod <xlinx@icloud.com>
2025-02-12 15:38:29 +0800

    sota v2 ok

--- pyApp/sacoMain.py
+++ pyApp/sacoMain.py
@@ -7,15 +7,15 @@ import datetime
 import glob
 from threading import Thread
 import cv2
-import numpy as np
+import numpy
 from ultralytics import YOLO
 # from SotaTool_decade_tw import run_sota
 # from shapely.geometry import Polygon
 #############
 from skimage import (restoration)
 import warnings
-import numpy as np
-from skimage.feature import greycomatrix, greycoprops
+# import numpy as np
+from skimage.feature import graycomatrix, graycoprops
 import cv2
 import os
 import matplotlib.pyplot as plt
@@ -47,16 +47,18 @@ class SOTA_ALL_VAR():
     offset_val = 0
     ballsize_val:int = 0
     max_length_val: int = 0
-    segments=0
+    segments=1
     background = 0
     skip_graph_save_time = 0
     output_Interpolated_data = 0
-    var1=background
-    var3 = skip_graph_save_time
-    var2=segments
-    var4 = output_Interpolated_data
-    tilevaly=0
-    tilevalx=0
+    tilevaly:int=10
+    tilevalx:int=10
+    tiling_dir:str= '/segments'
+    # var1=background
+    # var3 = skip_graph_save_time
+    # var2=segments
+    # var4 = output_Interpolated_data
+
 
 # Path_X=dict()
 # for f in [e.value for e in fNameX]:
@@ -70,6 +72,7 @@ for f in fNameX:
 DECADE_MODEL_PATH=fNameX.modelAI['pathObjX'].joinpath(DECADE_MODEL)
 SOTA_ALL_VAR.inx = fNameX.output['pathStrX']
 SOTA_ALL_VAR.outx = fNameX.sota['pathStrX']
+SOTA_ALL_VAR.tiling_dir= fNameX.output['pathStrX'] + '/segments'
 
 # print('[][][]',list(map(dict, fNameX)))
 if not os.path.exists(DECADE_MODEL_PATH):
@@ -96,7 +99,7 @@ def run_sota(ALL_VAR):
     num_cores = multiprocessing.cpu_count()
 
     # CoV-formula, input from user translations and output folder creation
-    cv = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
+    cv = lambda x: numpy.std(x, ddof=1) / numpy.mean(x) * 100
     input_dir = SOTA_ALL_X.inx
     output_dir = SOTA_ALL_X.outx
     if not os.path.exists(output_dir):
@@ -120,84 +123,87 @@ def run_sota(ALL_VAR):
     else:
         max_length_val = round(float(SOTA_ALL_X.max_length_val) * voxel)
     # print('[][][]prompt',prompt)
-    x = np.arange(1, prompt, 1)
+    x = numpy.arange(1, prompt, 1)
+    print('x1',x)
     x = x * (1 / voxel)
+    print('x2', x)
     max_x=prompt* (1 / voxel)
-    x_new = np.linspace((1 / voxel), max_x, prompt * 100)
+    x_new = numpy.linspace((1 / voxel), max_x, prompt * 100)
     # print('[][][0]x_new', x_new)
     distances = [*range(1, prompt, 1)]
     distances2 = [*range(0, prompt - 1, 1)]
+    print('distances', distances)
+    print('distances2', distances2)
     angles = [*range(1, 181, 1)]
+    print('angles', angles)
     # print('[][][]distances', distances)
     # print('[][][]distances2', distances2)
     # print('[][][]angles', angles)
     # Tiling
-    if SOTA_ALL_X.var2 == 1:
-        tiling_dir = input_dir + '/segments'
-        if not os.path.exists(tiling_dir):
-            os.makedirs(tiling_dir)
-        files = glob.glob(tiling_dir + '/*')
+    if SOTA_ALL_X.segments == 1:
+
+        if not os.path.exists(SOTA_ALL_VAR.tiling_dir):
+            os.makedirs(SOTA_ALL_VAR.tiling_dir)
+        files = glob.glob(os.path.join(SOTA_ALL_VAR.tiling_dir , '*'))
         for f in files:
             os.remove(f)
-        tileval_y = SOTA_ALL_X.tilevaly
-        tileval_x = SOTA_ALL_X.tilevalx
-        for img in glob.glob(input_dir + "/*.tif"):
-            image = cv2.imread(img)
-            image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            numrows = int(tileval_x)
-            numcols = int(tileval_y)
-            height = int(image2.shape[0] / numrows)
-            width = int(image2.shape[1] / numcols)
-            for row in range(numrows):
-                for col in range(numcols):
+
+        for img in glob.glob(os.path.join(input_dir , "*.tif")):
+            image2 = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2GRAY)
+            print('img[folderLen:]',img[folderLen:])
+            height = int(image2.shape[0] / SOTA_ALL_X.tilevalx)
+            width = int(image2.shape[1] / SOTA_ALL_X.tilevaly)
+            for row in range(SOTA_ALL_X.tilevalx):
+                for col in range(SOTA_ALL_X.tilevaly):
                     y0 = row * height
                     y1 = y0 + height
                     x0 = col * width
                     x1 = x0 + width
-                    cv2.imwrite(tiling_dir + '/' + img[folderLen:].replace('.tif', '') + '_tile_%d%d.tif' % (row, col), image2[y0:y1, x0:x1])
+                    cv2.imwrite(os.path.join(SOTA_ALL_VAR.tiling_dir,os.path.basename(img)+'_tile_%d_%d' % (row, col))+Path(img).suffix  ,
+                                image2[y0:y1, x0:x1])
 
     def algorithm(iAngle):
         rotateI = imutils.rotate_bound(gray_image, iAngle)
-        glcm = greycomatrix(rotateI,
+        glcm = graycomatrix(rotateI,
                             distances=distances,
                             angles=[0],
                             symmetric=False,
                             normed=False)[noise_red_val:, noise_red_val:, :, :]
 
-        correlation = greycoprops(glcm, 'correlation')
+        correlation = graycoprops(glcm, 'correlation')
         df = pd.DataFrame(correlation)
         df2[iAngle] = df
-        # asm = greycoprops(glcm[:, :, 0:1, :], 'ASM').mean()
-        # homogeneity = greycoprops(glcm[:, :, 0:1, :], 'homogeneity').mean()
+        asm = graycoprops(glcm[:, :, 0:1, :], 'ASM').mean()
+        homogeneity = graycoprops(glcm[:, :, 0:1, :], 'homogeneity').mean()
 
 
     # List files:
-    if SOTA_ALL_X.var2 == 1:
-        files = glob.glob(input_dir + "/*.tif") + glob.glob(tiling_dir + "/*.tif")
+    if SOTA_ALL_X.segments == 1:
+        files = glob.glob(os.path.join(input_dir , "*.tif")) + glob.glob(os.path.join(SOTA_ALL_VAR.tiling_dir , "/*.tif"))
     else:
-        files = glob.glob(input_dir + "/*.tif")
+        files = glob.glob(os.path.join(input_dir , "*.tif"))
 
     # Analysis GLCM and sarcomere length, loop over folder
     for img in tqdm(files):
         image = cv2.imread(img)
-        image_ID = '/' + img[folderLen:].replace('.tif', '').replace('\\', '').replace('/segments', '')
-        # image = cv2.imread('C:/Users/stein/Documents/Python/Raw data for article/Matthew cells/TID/Image005_TID_ch01.tif')
-
+        # image_ID = '/' + img[folderLen:].replace('.tif', '').replace('\\', '').replace('/segments', '')
+        image_ID = Path(img).name.split('.')[0]
+        print('image_ID',image_ID)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Background subtraction
-        if SOTA_ALL_X.var1 == 1:
+        if SOTA_ALL_X.background == 1:
             background = restoration.rolling_ball(gray_image, radius=int(ballsize_val))
             gray_image = gray_image - background
 
         # prepare dataframes for loop
         df2 = pd.DataFrame(index=distances2, columns=angles)
-        df2 = df2.astype(np.float32)
+        df2 = df2.astype(numpy.float32)
 
         # multithread function of GLCM and save df2 to .csv
         Parallel(n_jobs=num_cores, prefer="threads")(delayed(algorithm)(iAngle) for iAngle in angles)
 
-        df2.to_csv(output_dir + image_ID + '_corr' + ".csv")
+        df2.to_csv(os.path.join(output_dir,image_ID + '_corr' + ".csv"))
 
         # Peak detection in and amplitude calculation in all curves
         # maximum sarcomere length taken into account here
@@ -205,9 +211,9 @@ def run_sota(ALL_VAR):
         for column in df2:
             ser = pd.Series(df2[column][:max_length_val])
             indices = find_peaks(ser)[0]
-            flag = not np.any(indices)
+            flag = not numpy.any(indices)
             if flag:
-                indices = np.array([1, 1])
+                indices = numpy.array([1, 1])
             peak = pd.Series(ser[indices[0]] - min(ser[0:indices[0]]))
             peaks.append(peak)
 
@@ -218,16 +224,16 @@ def run_sota(ALL_VAR):
         # So now we have the angle and SOS of the highest curve. Now we need to extract the sarcomere length from df2
         # using the splined data
         max_curve = pd.Series(df2[SOS_max[0]])
-        y = np.array(max_curve.values.tolist())
+        y = numpy.array(max_curve.values.tolist())
         f = CubicSpline(x, y, bc_type='natural')
         y_new = f(x_new)
         scaled_maxcurve = pd.DataFrame(pd.Series(x_new), columns=['Offset'])
         scaled_maxcurve['Correlation'] = y_new.tolist()
-        if SOTA_ALL_X.var4 == 1:
+        if SOTA_ALL_X.output_Interpolated_data == 1:
             scaled_maxcurve.to_csv(output_dir + image_ID + 'int' + ".csv")
 
         peak_s_length = find_peaks(y_new)[0]
-        flag = not np.any(peak_s_length)
+        flag = not numpy.any(peak_s_length)
         if flag:  # these parameters should be 0 if there are no peaks in the splined graph
             s_length = 'NA'
             angle_val = 'NA'
@@ -238,16 +244,16 @@ def run_sota(ALL_VAR):
             SOS = SOS_max[1]
 
         # Sure, just calculate the Coefficient of Variation real quick
-        CoV_offset = find_peaks(np.array(df2[SOS_max[0]]))
-        if not np.any(CoV_offset[0]):
+        CoV_offset = find_peaks(numpy.array(df2[SOS_max[0]]))
+        if not numpy.any(CoV_offset[0]):
             CoV = 'NA'
-            CoV_offset = np.array([[0.0], [0]], dtype=np.int64)
+            CoV_offset = numpy.array([[0.0], [0]], dtype=numpy.int64)
         else:
             CoV = cv(df2.iloc[[CoV_offset[0][0] + 1]].squeeze()[1:])
 
         # Create epic graph which took way too long to design, fml
         # Melt df, order hues on CoV-offset in a list, choose palette, plot all lines with seaborn
-        if SOTA_ALL_X.var3 == 0:
+        if SOTA_ALL_X.skip_graph_save_time == 0:
             df2.insert(loc=0, column='Offset', value=x)
             df2 = df2.drop([90], axis=1)
             df2 = df2.drop([180],  axis=1)
@@ -273,25 +279,27 @@ def run_sota(ALL_VAR):
             plt.xlabel('Offset [um]')
             plt.ylabel('Correlation')
             fig = plt.gcf()
-            fig.savefig(output_dir + image_ID + '_output' + '.tif')
+
+            fig.savefig(os.path.join(output_dir , image_ID + '_output' + '.tif'))
 
         # Create new GLCM with furthest pixel offset for ASM and Homogeneity
-        glcm = greycomatrix(gray_image,
+        glcm = graycomatrix(gray_image,
                             distances=[max(distances2)],
                             angles=[0],
                             symmetric=False,
                             normed=False)[noise_red_val:, noise_red_val:, :, :]
-        asm = greycoprops(glcm[:, :, 0:1, :], 'ASM').mean()
-        homogeneity = greycoprops(glcm[:, :, 0:1, :], 'homogeneity').mean()
+        asm = graycoprops(glcm[:, :, 0:1, :], 'ASM').mean()
+        homogeneity = graycoprops(glcm[:, :, 0:1, :], 'homogeneity').mean()
 
         ### Compile useful output in summary excel file
         data = [
-            [img[folderLen:].replace('.tif', '').replace('\\', '').replace('segments', ''), s_length, angle_val, SOS, CoV,
+            [ Path(img).name, s_length, angle_val, SOS, CoV,
              asm, homogeneity]]
         final = pd.DataFrame(data, columns=['Name', 'Sarcomere length', 'Angle', 'Sarcomere Organization Score',
                                             'Alignment index', 'Uniformity', 'Homogeneity'])
         Summary = Summary._append(final)
-        Summary.to_csv(output_dir + "/Summary" +  ".csv")
+
+        Summary.to_csv(os.path.join(output_dir , Path(img).name + "_Summary" +  ".csv"))
 
 def directory_modified(dir_path, poll_timeout=1):
     init_mtime = os.stat(dir_path).st_mtime
@@ -302,7 +310,7 @@ def directory_modified(dir_path, poll_timeout=1):
             print(datetime.datetime.now(),"[1][decade.tw][monitor][different]input=",fNameX.input,", output=",fNameX.output)
             allImages = []
             for ext in ('*.gif', '*.png', '*.jpg','*.tif'):
-                allImages.extend(glob.glob(fNameX.input['pathStrX']+os.sep+ ext))
+                allImages.extend(glob.glob(os.path.join(fNameX.input['pathStrX'],ext)))
             # allImages=glob.glob(att_dir_input+os.sep+'*.jpg')
             print(datetime.datetime.now(), "[2][decade.tw][monitor][different]allImages=", allImages)
             # yolo_decade(None,None,allImages)
@@ -329,9 +337,9 @@ def thread_safe_predict(yolo_model, image_path):
         Path(image_path).rename(fNameX.processed['pathObjX'].joinpath(Path(image_path).name))
 
         for index in range(len(masks)):
-            img = np.copy(result.orig_img)
-            b_mask = np.zeros(img.shape[:2], np.uint8)
-            contour = masks[index].xy[0].astype(np.int32).reshape(-1, 1, 2)
+            img = numpy.copy(result.orig_img)
+            b_mask = numpy.zeros(img.shape[:2], numpy.uint8)
+            contour = masks[index].xy[0].astype(numpy.int32).reshape(-1, 1, 2)
             cv2.drawContours(b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
             mask3ch = cv2.cvtColor(b_mask, cv2.COLOR_GRAY2BGR)
             isolated = cv2.bitwise_and(mask3ch, img)
@@ -346,17 +354,19 @@ def yolo_decade(raw=None,imagePath=None,allImages=None):
         threadX.start()
         threadX.join()
         # run_sota(fNameX.output['pathStrX'],fNameX.sota['pathStrX'])
+        SOTA_ALL_VAR.voxelvalue=1.1
+        LIST_SOTA()
+        Thread(target=run_sota, args=[SOTA_ALL_VAR] ).start()
     else:
         yolo_results = yolo_model(allImages)
 
-    SOTA_ALL_VAR.voxelvalue=1.1
-    LIST_SOTA()
-    Thread(target=run_sota, args=[SOTA_ALL_VAR] ).start()
+
     return resultFinal
 
 
 
-directory_modified(fNameX.input['pathStrX'], 5)
+# directory_modified(fNameX.input['pathStrX'], 5)
+Thread(target=run_sota, args=[SOTA_ALL_VAR] ).start()
 # run_sota(fNameX.output['pathStrX'], fNameX.sota['pathStrX'])
 
 #pip install ultralytics
@@ -364,3 +374,5 @@ directory_modified(fNameX.input['pathStrX'], 5)
 #pyinstaller -F sacoMain.py
 # pyinstaller -D sacoMain.py
 #pip install pyinstaller
+#pip uninstall -y -r <(pip freeze)
+#pipreqs --ignore .venv --force --savepath requirments.now.txt ./
